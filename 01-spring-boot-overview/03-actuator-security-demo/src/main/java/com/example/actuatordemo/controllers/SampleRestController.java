ae5911b65b33b5449f03a200d44aeebe594a4f79
pformela <patrykformela.00@gmail.com>
2023-12-14 20:55:49 +0100

    finished spring boot quick start section

---
+++ 01-spring-boot-overview/03-actuator-security-demo/src/main/java/com/example/actuatordemo/controllers/SampleRestController.java
@@ -0,0 +1,23 @@
+package com.example.actuatordemo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleRestController {
+
+    @GetMapping("/")
+    public String getHome() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5 km!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day!";
+    }
+}
